4e2915d5d1971ee8669037c4f400c275978bac0b
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-06-18 16:20:25 -0300

    fix: não consegue reconhecer duas rotas

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,13 +1,7 @@
-// src/utils/axiosInstance.ts
 import axios from "axios";
 
-const isLocalhost = window.location.hostname === "localhost";
-const baseURL = isLocalhost
-  ? import.meta.env.VITE_API_URL_DEV
-  : import.meta.env.VITE_API_URL_PROD;
-
 const api = axios.create({
-  baseURL,
+  baseURL: "https://docrh.onrender.com/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
