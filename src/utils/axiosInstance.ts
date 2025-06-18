552e518928569086827c6769db39918fcf4bf7a4
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-06-18 15:39:07 -0300

    fix: arrumei uma lógica para rodar o projeto tanto local quanto no servidor

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,13 @@
+// src/utils/axiosInstance.ts
 import axios from "axios";
 
+const isLocalhost = window.location.hostname === "localhost";
+const baseURL = isLocalhost
+  ? import.meta.env.VITE_API_URL_DEV
+  : import.meta.env.VITE_API_URL_PROD;
+
 const api = axios.create({
-  baseURL: "https://docrh.onrender.com/",
+  baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
