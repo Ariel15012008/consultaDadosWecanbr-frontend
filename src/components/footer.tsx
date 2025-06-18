04e2e80d00a664ab9b089fad41a64afc045d30c0
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-06-18 14:39:21 -0300

    fix: tirei o link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
 import { FaWhatsapp, FaInstagram, FaFacebookF } from "react-icons/fa";
 
 export default function Footer() {
