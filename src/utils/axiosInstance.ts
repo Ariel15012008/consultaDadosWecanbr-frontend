141f6a23c4c5e161ea02ded0536bf9177baff8bc
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-06-23 16:57:37 -0300

    fix: arrumando as requisições

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
+// const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
+
+const VITE_API_ENVIRONMENT: string = "prod"
+const VITE_API_URL_DEV: string = "http://localhost:8000/"
+const VITE_API_URL_PROD: string ="https://docrh.onrender.com/"
+
+const url = VITE_API_ENVIRONMENT == "prod" ? VITE_API_URL_PROD : VITE_API_URL_DEV;
 
 const api = axios.create({
   baseURL: url,
