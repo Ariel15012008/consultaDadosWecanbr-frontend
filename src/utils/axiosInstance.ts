121f6ac9a226bbb0fb3b80003ddc2d6fba782648
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-06-27 14:24:41 -0300

    fix: arrumando lógica

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
 
-const VITE_API_ENVIRONMENT: string = "dev"
+const VITE_API_ENVIRONMENT: string = "prod"
 const VITE_API_URL_DEV: string = "http://localhost:8000/"
 const VITE_API_URL_PROD: string ="https://docrh.onrender.com/"
 
@@ -10,6 +10,7 @@ const url = VITE_API_ENVIRONMENT == "prod" ? VITE_API_URL_PROD : VITE_API_URL_DE
 
 const api = axios.create({
   baseURL: url,
+  timeout: 5000, 
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
