ebed2a1492eb61dedd159c03d4b0c096daf76c60
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-07-28 13:44:35 -0300

    fix: coloquei em produção

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
 
-const VITE_API_ENVIRONMENT: string = "dev";
+const VITE_API_ENVIRONMENT: string = "prod";
 const VITE_API_URL_DEV: string = "http://localhost:8000/";
 const VITE_API_URL_PROD: string = "https://docrh.onrender.com/";
 
