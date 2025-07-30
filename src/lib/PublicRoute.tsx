5342731a155630645681558888941c3abfe4c2a9
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-07-30 15:24:11 -0300

    fix: teste

--- src/lib/PublicRoute.tsx
+++ src/lib/PublicRoute.tsx
@@ -4,6 +4,11 @@ import Cookies from "js-cookie"
 export function PublicRoute() {
   const loggedUser = Cookies.get("logged_user")
 
+  if(loggedUser === "true") {
+    // Se não houver usuário logado, permite o acesso às rotas públicas
+    return <Navigate to="/" replace />
+  }
+
   const isAuthenticated = !!loggedUser
 
   if (isAuthenticated) {
