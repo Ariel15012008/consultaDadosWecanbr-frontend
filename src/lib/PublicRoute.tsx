8e6af62d5f8158f1d380f62c0e4f872d3897f963
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-07-30 15:20:14 -0300

    fix: teste

--- src/lib/PublicRoute.tsx
+++ src/lib/PublicRoute.tsx
@@ -1,47 +1,14 @@
-import { useEffect } from "react";
-import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import api from "@/utils/axiosInstance";
+import { Navigate, Outlet } from 'react-router-dom'
+import Cookies from "js-cookie"
 
 export function PublicRoute() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const pathname = location.pathname;
+  const loggedUser = Cookies.get("logged_user")
 
-  const loggedUser = Cookies.get("logged_user");
-  const now = Date.now();
-  const twoMinutes = 2 * 60 * 1000;
-
-  const isAuthenticated = !!loggedUser;
-
-  useEffect(() => {
-    const verificarToken = async () => {
-      if (loggedUser) {
-        const loggedTime = parseInt(loggedUser);
-        const timeDiff = now - loggedTime;
-
-        if (timeDiff > twoMinutes) {
-          try {
-            await api.post("/user/refresh");
-            localStorage.setItem("logged_user", Date.now().toString());
-          } catch (err) {
-            localStorage.removeItem("access_token");
-            localStorage.removeItem("logged_user");
-            return; // Permite continuar na rota pública como anônimo
-          }
-        }
-
-        // Já autenticado? então redireciona para home.
-        navigate("/", { replace: true });
-      }
-    };
-
-    verificarToken();
-  }, [loggedUser, pathname, navigate, now]);
+  const isAuthenticated = !!loggedUser
 
   if (isAuthenticated) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/" replace />
   }
 
-  return <Outlet />;
+  return <Outlet />
 }
