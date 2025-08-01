e3329e193dff4d211018d2aebd9605eeb595ec75
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-08-01 18:33:36 -0300

    fix: arrumando

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,50 @@
 // src/App.tsx
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { UserProvider } from "@/contexts/UserContext"
 
-import Cadastro            from "@/pages/register/Cadastro"
-import Login               from "@/pages/login/Login"
 import Home                from "@/pages/home/Home"
-import IdTemplate          from "@/pages/documentos/[id_tipo]"
-import PreviewPDF          from "@/pages/documento/preview"
+import Login               from "@/pages/login/Login"
+import Cadastro            from "@/pages/register/Cadastro"
 import ForgotPasswordPage  from "@/pages/password/ForgotPasswordPage"
 import ResetPasswordPage   from "@/pages/resetPassword/ResetPasswordPage"
+import IdTemplate          from "@/pages/documentos/[id_tipo]"
+import PreviewPDF          from "@/pages/documento/preview"
 
-import { PublicRoute }     from "./lib/PublicRoute"
-import { ProtectedRoute }  from "./lib/ProtectedRoute"
+import { PublicRoute }    from "./lib/PublicRoute"
+import { ProtectedRoute } from "./lib/ProtectedRoute"
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
+      <Routes>
+        {/*  
+          1) Rota pública geral: Home em "/"   
+          2) Rotas de autenticação em "/login", "/register", etc.
+        */}
+        <Route element={<PublicRoute />}>
+          <Route path="/"                element={<Home />} />
+          <Route path="/login"           element={<Login />} />
+          <Route path="/register"        element={<Cadastro />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password"  element={<ResetPasswordPage />} />
+        </Route>
 
-          {/* 1) LANDING PAGE / PÚBLICA */}
-          <Route path="/" element={<Home />} />
+        {/*
+          3) Rotas protegidas: só essas duas
+          - /documentos
+          - /documento/preview
+        */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/documentos"            element={<IdTemplate />} />
+          <Route path="/documento/preview"     element={<PreviewPDF />} />
+        </Route>
 
-          {/* 2) AUTENTICAÇÃO */}
-          <Route element={<PublicRoute />}>
-            <Route path="login"           element={<Login />} />
-            <Route path="register"        element={<Cadastro />} />
-            <Route path="forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="reset-password"  element={<ResetPasswordPage />} />
-          </Route>
-
-          {/* 3) ROTAS PROTEGIDAS */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="documentos"               element={<IdTemplate />} />
-            <Route path="documento/preview/:id"    element={<PreviewPDF />} />
-          </Route>
-
-          {/* 4) QUALQUER OUTRA URL vai para “/” */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-
-        </Routes>
-      </BrowserRouter>
+        {/*
+          4) Qualquer outra URL volta pra "/" (Home)
+        */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </UserProvider>
   )
 }
