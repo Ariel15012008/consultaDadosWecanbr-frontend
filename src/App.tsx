54fda49d502b61bd6b5faa63e7523239469ea171
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-08-01 18:31:56 -0300

    fix: arrumando

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,49 @@
 // src/App.tsx
 
-import { Routes, Route, Navigate } from "react-router-dom";
-import { UserProvider } from "@/contexts/UserContext";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { UserProvider } from "@/contexts/UserContext"
 
-import Cadastro from "@/pages/register/Cadastro";
-import Login from "@/pages/login/Login";
-import Home from "@/pages/home/Home";
-import IdTemplate from "@/pages/documentos/[id_tipo]";
-import PreviewPDF from "@/pages/documento/preview";
-import ForgotPasswordPage from "@/pages/password/ForgotPasswordPage";
-import ResetPasswordPage from "@/pages/resetPassword/ResetPasswordPage";
+import Cadastro            from "@/pages/register/Cadastro"
+import Login               from "@/pages/login/Login"
+import Home                from "@/pages/home/Home"
+import IdTemplate          from "@/pages/documentos/[id_tipo]"
+import PreviewPDF          from "@/pages/documento/preview"
+import ForgotPasswordPage  from "@/pages/password/ForgotPasswordPage"
+import ResetPasswordPage   from "@/pages/resetPassword/ResetPasswordPage"
 
-import { PublicRoute } from "./lib/PublicRoute";
-import { ProtectedRoute } from "./lib/ProtectedRoute";
+import { PublicRoute }     from "./lib/PublicRoute"
+import { ProtectedRoute }  from "./lib/ProtectedRoute"
 
 function App() {
   return (
     <UserProvider>
-      <Routes>
-        {/* 1) ROTAS PÚBLICAS */}
-        <Route element={<PublicRoute />}>
-          {/* "/" agora é sua landing page */}
+      <BrowserRouter>
+        <Routes>
+
+          {/* 1) LANDING PAGE / PÚBLICA */}
           <Route path="/" element={<Home />} />
 
-          {/* acesso ao fluxo de autenticação */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Cadastro />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-        </Route>
+          {/* 2) AUTENTICAÇÃO */}
+          <Route element={<PublicRoute />}>
+            <Route path="login"           element={<Login />} />
+            <Route path="register"        element={<Cadastro />} />
+            <Route path="forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="reset-password"  element={<ResetPasswordPage />} />
+          </Route>
 
-        {/* 2) ROTAS QUE EXIGEM AUTENTICAÇÃO */}
-        <Route element={<ProtectedRoute />}>
-          {/* só estes dois paths ficam aqui */}
-          <Route path="/documentos" element={<IdTemplate />} />
-          <Route path="/documento/preview" element={<PreviewPDF />} />
-        </Route>
+          {/* 3) ROTAS PROTEGIDAS */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="documentos"               element={<IdTemplate />} />
+            <Route path="documento/preview/:id"    element={<PreviewPDF />} />
+          </Route>
 
-        {/* 3) QUALQUER OUTRA ROTA volta para "/" */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          {/* 4) QUALQUER OUTRA URL vai para “/” */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+
+        </Routes>
+      </BrowserRouter>
     </UserProvider>
-  );
+  )
 }
 
-export default App;
+export default App
