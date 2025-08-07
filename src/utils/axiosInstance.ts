730cbef0db65c958f3732ddd55bd32d6c310ad96
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-08-07 17:32:15 -0300

    fix: mudando url de requisições

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const VITE_API_ENVIRONMENT: string = "prod";
 const VITE_API_URL_DEV: string = "http://localhost:8000/";
-const VITE_API_URL_PROD: string = "http://3.17.64.91:8000/";
+const VITE_API_URL_PROD: string = "https://rh.ziondocs.com.br/";
 
 const url =
   VITE_API_ENVIRONMENT == "prod" ? VITE_API_URL_PROD : VITE_API_URL_DEV;
