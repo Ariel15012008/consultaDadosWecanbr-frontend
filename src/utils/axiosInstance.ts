344fdea4b0c892642f9ce81c388df26abef67f19
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2025-08-08 13:55:27 -0300

    fix: ajustando lógica de urls

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-// const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
-
-const VITE_API_ENVIRONMENT: string = "prod";
-const VITE_API_URL_DEV: string = "http://localhost:8000/";
-const VITE_API_URL_PROD: string = "https://rh.ziondocs.com.br/";
-
-const url =
-  VITE_API_ENVIRONMENT == "prod" ? VITE_API_URL_PROD : VITE_API_URL_DEV;
+const url = import.meta.env.VITE_API_ENVIRONMENT == "prod" ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
 
 const api = axios.create({
   baseURL: url,
